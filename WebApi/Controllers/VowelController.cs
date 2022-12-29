a1e2b913837ff54c228dd0b7a391ed26ee834b7d
Batuhan Akçagöz <akcagozbatuhan@gmail.com>
2022-12-29 11:39:58 +0300

    Vowel Histogram Controller Post created

---
+++ WebApi/Controllers/VowelController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+using WebApi.Models.Dto;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class VowelController : ControllerBase
+    {
+
+        [HttpPost("text")]
+        public async Task<ActionResult<VowelDTO>> GetString(VowelTxt vowelTxt)
+        {
+            VowelDTO voweldto = new VowelDTO();
+
+            voweldto.YourText = vowelTxt.txt;
+            if (vowelTxt.nat == "tr")
+            {
+                voweldto.Txttr = VowelChrs.txttr;
+                voweldto.Lang = "Türkçe";
+                voweldto.Vowels = vowelTxt.txt.Where(x => VowelChrs.txttr.Contains(x)).ToList();
+            }
+            else if (vowelTxt.nat == "en")
+            {
+                voweldto.Txttr = VowelChrs.txten;
+                voweldto.Lang = "English";
+                voweldto.Vowels = vowelTxt.txt.Where(x => VowelChrs.txten.Contains(x)).ToList();
+            }
+
+            return Ok(voweldto);
+        }
+    }
+}
